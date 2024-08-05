a9ca4b204aad4026b5dbb86333f869b350194b27
ryu <jeungwon28@gmail.com>
2024-08-05 11:25:37 +0900

    conn 값 위치 변경

--- src/main/java/com/programmers/jdbcproject/Main.java
+++ src/main/java/com/programmers/jdbcproject/Main.java
@@ -1,6 +1,11 @@
 package com.programmers.jdbcproject;
 
 public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = ""; // 데이터베이스 사용자명
+    private static final String PASSWORD = ""; // 데이터베이스 비밀번호
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
